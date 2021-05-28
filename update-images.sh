b6d2723d16a98cac11710770a0a6ada3731d8473
Guido Stein <guido@liveby.com>
2021-05-28 11:01:14 -0400

    👌 IMPROVE: update images

--- update-images.sh
+++ update-images.sh
@@ -3,5 +3,5 @@
 #Description: A script for image manipulation
 
 find . -name "*.webp" -exec sh -c 'cwebp -q 50 "$1" -o "${1%.webp}.webp"' _ {} \;
-find . -name "*.png" -exec sh -c 'cwebp -q 50 "$1" -o "${1%.png}.webp"' _ {} \;
+find . -name "*.webp" -exec sh -c 'cwebp -q 50 "$1" -o "${1%.webp}.webp"' _ {} \;
 # find /the/path -depth -name "*.webp" -exec sh -c 'cwebp -q 50 "$1" "${1%.webp}.webp"' _ {} \;
